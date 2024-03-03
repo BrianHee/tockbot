2bd72a5608ff7144242ac4b9cd7098c6766c49a1
Brian Kim <gittybrian@gmail.com>
2024-03-02 21:15:00 -0500

    complete tockbot functionality

--- tockbot/main.py
+++ tockbot/main.py
@@ -9,7 +9,10 @@ from reservation import RESERVATION
 import time
 
 RESERVATION_HELD = False
+REFRESH_DELAY = 1
 WAIT_DELAY = 1000
+FORMAT_YMD = '%Y-%m-%d'
+FORMAT_IMP = '%I:%M %p'
 
 class TockBot():
   def __init__(self):
@@ -20,37 +23,71 @@ class TockBot():
   
   def reserve(self):
     nondate = self.get_nondate()
-    self.driver.get('https://www.exploretock.com/%s/search?date=%s&size=%s&time=%s' % (RESERVATION.location, nondate, RESERVATION.size, RESERVATION.start_time.replace(':', '%3A')))
-    WebDriverWait(self.driver, WAIT_DELAY).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.SearchBarMonths')))
+    start_time = datetime.strptime(RESERVATION.start_time, FORMAT_IMP).strftime('%H:%M')
     while not RESERVATION_HELD:
+      time.sleep(REFRESH_DELAY)
+      self.driver.get('https://www.exploretock.com/%s/search?date=%s&size=%s&time=%s' % (RESERVATION.location, nondate, RESERVATION.size, start_time))
+      WebDriverWait(self.driver, WAIT_DELAY).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.SearchBarMonths')))
       self.search_dates()
-      self.quit()
     time.sleep(600)
 
   def search_dates(self):
-    global RESERVATION_HELD
-    days = self.driver.find_elements(By.CSS_SELECTOR, 'button.ConsumerCalendar-day.is-available')
+    month = self.driver.find_element(By.CSS_SELECTOR, 'div.ConsumerCalendar-month')
+    days = month.find_elements(By.CSS_SELECTOR, 'button.ConsumerCalendar-day.is-available')
+    if not days:
+      print('No available dates found for this month yet.')
+      return
     for day in days:
       aria_label = day.get_attribute("aria-label")
       if aria_label in RESERVATION.dates:
-        print(aria_label)
-    
-    RESERVATION_HELD = True
+        print(f'Availabilities for {aria_label} found - checking times.')
+        day.click()
+        if self.search_times():
+          break
+        else:
+          print(f'No time available for {aria_label}.')
+  
+  def search_times(self):
+    global RESERVATION_HELD
+    experience = self.search_experiences()
+    if not experience:
+      return False
+    for time_slot in experience.find_elements(By.XPATH, "//div[@data-testid='booking-card-collapse']"):
+      span = time_slot.find_element(By.CSS_SELECTOR, 'span.Consumer-resultsListItemTime').find_element(By.CSS_SELECTOR, 'span')
+      date_time = datetime.strptime(span.text, FORMAT_IMP)
+      if datetime.strptime(RESERVATION.start_time, FORMAT_IMP) <= date_time <= datetime.strptime(RESERVATION.end_time, FORMAT_IMP):
+        print('Time found for: %s. Please complete the reservation.' % (date_time.strftime(FORMAT_IMP)))
+        time_slot.click()
+        RESERVATION_HELD = True
+        return True
+    return False
+
+  def search_experiences(self):
+    experiences = self.driver.find_elements(By.CSS_SELECTOR, 'button.Consumer-reservationLink')
+    if RESERVATION.experience:
+      for experience in experiences:
+        h3 = experience.find_element(By.CSS_SELECTOR, 'h3.Consumer-reservationHeading')
+        if h3.text.lower() == RESERVATION.experience.lower():
+          return experience
+    else:
+      return experiences[0]
   
   def get_nondate(self):
-    first_date_time = datetime.strptime(RESERVATION.dates[0],'%Y-%m-%d')
-    last_date_time = datetime.strptime(RESERVATION.dates[-1],'%Y-%m-%d')
-    first_of_month = datetime(first_date_time.year, first_date_time.month, 1).strftime('%Y-%m-%d')
+    first_date_time = datetime.strptime(RESERVATION.dates[0], FORMAT_YMD)
+    first_of_month = datetime(first_date_time.year, first_date_time.month, 1).strftime(FORMAT_YMD)
     if first_of_month not in RESERVATION.dates:
       return first_of_month
     else:
-      return (last_date_time + timedelta(days=1)).strftime('%Y-%m-%d')
+      non_date = first_of_month
+      while non_date in RESERVATION.dates:
+        non_date = (datetime.strptime(non_date, FORMAT_YMD) + timedelta(days=1)).strftime(FORMAT_YMD)
+      return non_date
 
   def quit(self):
     self.driver.quit()
 
 def run_attempt():
-  print('Starting reservation attempt for %s for the following days: %s in the month of %s.' % (RESERVATION.location, (', ').join(RESERVATION.days), RESERVATION.month))
+  print('Starting reservation attempt for %s for the following days: %s.' % (RESERVATION.location, (', ').join(RESERVATION.dates)))
   tock_bot = TockBot()
   tock_bot.reserve()
   tock_bot.quit()
--- tockbot/reservation.py
+++ tockbot/reservation.py
@@ -4,6 +4,7 @@ RESERVATION = SimpleNamespace(
   location='quintonil',
   dates=['2024-04-08', '2024-04-09', '2024-04-10'],
   start_time='18:00',
-  end_time='20:00',
-  size=4
+  end_time='22:00',
+  size=4,
+  experience='MAIN DINING ROOM RESERVATION DEPOSIT'
 )
